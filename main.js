f01a020845beecad99391c8fbbc6fe9af0d5e316
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-09-18 21:36:48 +0530

    style update

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
- 
+const menuBtn = document.querySelector('.menu-btn')
+const menuNav = document.querySelector('.nav-mobile-links')
+const menuNavLink = document.querySelectorAll('.link')
+const navbarLink = document.querySelectorAll('.link')
+
+const toogleNav = () => {
+  // const menuDisplay = menuNav.className
+  menuNav.style.display === 'none'
+    ? (menuNav.style.display = 'flex')
+    : (menuNav.style.display = 'none')
+  console.log('clack', menuNav.style.display)
+}
+
+menuBtn.addEventListener('click', toogleNav)
+
+navbarLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    navbarLink.forEach((l) => (l.style.color = '#555454'))
+    link.style.color = '#000'
+  })
+})
+
+menuNavLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuNavLink.forEach((l) => (l.style.color = '#555454'))
+    link.style.color = '#000'
+    toogleNav()
+  })
+})
